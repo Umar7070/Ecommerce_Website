a5d5e2a9ef93f09c622cb93543c444eee58ecdc3
umar7070 <mohdumar707080@gmail.com>
2024-10-09 23:22:04 +0530

    working cotact page

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,12 +1,32 @@
-import React from "react";
-// import Link from "next/link";
+import React from 'react'
 
-const Contact = () => {
+const page = () => {
   return (
-    <>
-      <h1>contach pages</h1>
-    </>
-  );
-};
+    <div className='container'>
+      <form action="" className='flex  justify-center w-[50%] m-auto  flex-col ' >
+        <div className='grid grid-cols-2 gap-10 my-10'>
 
-export default Contact;
+        <div className=' '>
+          <input type="text" placeholder='enter your name' className='bg-gray-700 p-2   text-white rounded-lg  outline-none' />
+        </div>
+        <div className=''>
+          <input type="text" placeholder='enter your email' className='bg-gray-700 p-2 rounded-lg text-white  outline-none' />
+        </div>
+        <div className=''>
+          <input type="password" placeholder='enter your Password' className='bg-gray-700 p-2 rounded-lg text-white  outline-none' />
+        </div>
+        <div className=''>
+          <input type="password" placeholder='enter your Confirm Password' className='bg-gray-700 p-2 rounded-lg text-white   outline-none' />
+        </div>
+        </div>
+      <button className='bg-blue-500 p-1 rounded-lg text-white capitalize'>submit</button>
+
+
+      </form>
+
+      
+    </div>
+  )
+}
+
+export default page
